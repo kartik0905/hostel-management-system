e76d4fe53af239a5cb3cde0f67793029fde4f620
Kartik Garg <kartikamitgarg2005@gmail.com>
2026-04-03 18:58:29 +0530

    complaint models is created

---
+++ models/complaints.models.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const complaintSchema = new mongoose.Schema({
+    tenantId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    status: {
+        type: String,
+        enum: ['Pending', 'In-Progress', 'Resolved'],
+        default: 'Pending'
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('Complaint', complaintSchema);
